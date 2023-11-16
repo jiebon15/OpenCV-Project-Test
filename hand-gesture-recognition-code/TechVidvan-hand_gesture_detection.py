ede953e8050d39fe6928b776c214cba296b6198f
Ajie Prasetio <110334264+jiebon15@users.noreply.github.com>
2023-11-16 20:19:04 +0700

    Update TechVidvan-hand_gesture_detection.py

--- hand-gesture-recognition-code/TechVidvan-hand_gesture_detection.py
+++ hand-gesture-recognition-code/TechVidvan-hand_gesture_detection.py
@@ -5,19 +5,18 @@
 import cv2
 import numpy as np
 import mediapipe as mp
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 
 # initialize mediapipe
 mpHands = mp.solutions.hands
-hands = mpHands.hands(max_num_hands=1, min_detection_confidence=0.7)
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mpDraw = mp.solutions.drawing_utils
 
 # Load the gesture recognizer model
-model = load_model('mp_hand_gesture')
+model = load_model('D:/Programing/OpenCV-Project-Test/hand-gesture-recognition-code/mp_hand_gesture')
 
 # Load class names
-f = open('gesture.names', 'r')
+f = open('D:/Programing/OpenCV-Project-Test/hand-gesture-recognition-code/gesture.names', 'r')
 classNames = f.read().split('\n')
 f.close()
 print(classNames)
@@ -59,6 +58,7 @@ while True:
 
             # Predict gesture
             prediction = model.predict([landmarks])
+            
             # print(prediction)
             classID = np.argmax(prediction)
             className = classNames[classID]
