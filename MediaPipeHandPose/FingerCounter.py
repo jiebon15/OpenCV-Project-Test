5fd4c7a2718e226b6d5b3bb30da07fcc979c9e07
jiebon15 <110334264+jiebon15@users.noreply.github.com>
2023-11-23 11:14:15 +0700

    penyesuaian program FingerCounter.py

--- MediaPipeHandPose/FingerCounter.py
+++ MediaPipeHandPose/FingerCounter.py
@@ -9,16 +9,6 @@ cap = cv2.VideoCapture(1)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
-folderPath = "FingerImages"
-myList = os.listdir(folderPath)
-print(myList)
-overlayList = []
-for imPath in myList:
-    image = cv2.imread(f'{folderPath}/{imPath}')
-    # print(f'{folderPath}/{imPath}')
-    overlayList.append(image)
-
-print(len(overlayList))
 pTime = 0
 
 detector = htm.handDetector(detectionCon=0.75)
@@ -50,20 +40,6 @@ while True:
         # print(fingers)
         totalFingers = fingers.count(1)
         print(totalFingers)
-
-        h, w, c = overlayList[totalFingers - 1].shape
-        img[0:h, 0:w] = overlayList[totalFingers - 1]
-
-        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
-        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
-                    10, (255, 0, 0), 25)
-
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-
-    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
-                3, (255, 0, 0), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
