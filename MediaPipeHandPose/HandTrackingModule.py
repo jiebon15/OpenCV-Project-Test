349cacb03f589a5bd00d746af13fe4d4d5d8ad27
jiebon15 <110334264+jiebon15@users.noreply.github.com>
2023-11-23 11:35:00 +0700

    fix program FingerCounter

--- MediaPipeHandPose/HandTrackingModule.py
+++ MediaPipeHandPose/HandTrackingModule.py
@@ -12,8 +12,7 @@ class handDetector():
         self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
-                                        self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, int(self.detectionCon), self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
     def findHands(self, img, draw=True):
